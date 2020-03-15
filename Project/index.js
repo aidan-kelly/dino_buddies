c0bdc6a15a73fac9c95e1abfc8035bd1493006e8
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2020-03-15 04:29:53 -0600

    can now add groups

--- Project/index.js
+++ Project/index.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var app = express();
 var io = require('socket.io')(http);
+app.use(express.urlencoded())
 const mongo = require('mongodb')
 const MongoClient = require('mongodb').MongoClient;
 var http = require('http').createServer(app);
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+let client;
 
 
 app.get('/', async function(req, res){
-  let client = await main();
+  await main();
   let groups = await getGroups(client);  
-  let rooms = await getRooms(client, 'SENG513')
-  data = {rooms: rooms, groups: groups}
+  data = {groups: groups}
   console.log(data);
   res.render('index', data);
 });
 
+app.post('/new_group', async function(req, res){
+  console.log(req.body.name);
+  makeGroups(client, req.body.name)
+  res.render('index');
+});
+
 async function main(){
   const url = "mongodb+srv://admin:nxLMpNZw4Gxejkw1@cluster0-v7p6o.mongodb.net/test?retryWrites=true&w=majority";
-  const client = new MongoClient(url, {useUnifiedTopology: true});
+  client = new MongoClient(url, {useUnifiedTopology: true});
  
   try {
       // Connect to the MongoDB cluster
@@ -28,8 +35,6 @@ async function main(){
   } catch (e) {
       console.error(e);
   } 
-  //getRooms(client)
-  return client;
 }
 
 // these functions work asynconicly so you have to wait for them to finish before using them.
@@ -38,13 +43,16 @@ async function getGroups(client){
   return results;
 }
 
-async function getRooms(client, group_name){
-  let result = await client.db('discord-clone').collection('chat-room').find({"group": group_name}).toArray();
-  return result;
+async function makeGroups(client, group_name){
+  await client.db('discord-clone').collection('group').update(
+    // condition that this doesn't exist
+    {name: group_name},
+    // if it doesn't exist, implement this in addition
+    { $setOnInsert: {rooms: ['General', 'Work']}},
+    // if it does exist, don't implement the addition
+    {upsert: true}
+    )
 }
-
-
-
 
 
 app.listen(process.env.PORT || 3000 );
