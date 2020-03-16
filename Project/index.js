05beb7521afe6aab266441bc957a993a7d34eae4
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2020-03-16 16:15:37 -0600

    cleaning and added some css

--- Project/index.js
+++ Project/index.js
@@ -1,28 +1,66 @@
 var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 app.use(express.urlencoded())
 const mongo = require('mongodb')
 const MongoClient = require('mongodb').MongoClient;
-var http = require('http').createServer(app);
+const bodyParser = require('body-parser');
 app.use(express.static('public'))
+app.use(bodyParser())
 app.set('view engine', 'ejs')
 let client;
 
+io.on('connect', onConnect);
+
+
+async function onConnect(socket){
+  console.log('user connected')
+  socket.on('load-page', async function(e){
+    await main();
+    let groups = await getGroups(client);  
+    let data = {groups: groups}
+    console.log(data);
+    socket.emit('load-groups', data);
+  })
+
+  socket.on('create-group', async function(e){
+    console.log('group name: ', e)
+    makeGroups(client, e);
+    let groups = await getGroups(client);  
+    let data = {groups: groups}
+    console.log('reload users')
+    io.emit('reload-groups', data);
+  })
+
+
+}
 
 app.get('/', async function(req, res){
-  await main();
-  let groups = await getGroups(client);  
-  data = {groups: groups}
-  console.log(data);
-  res.render('index', data);
-});
-
-app.post('/new_group', async function(req, res){
-  console.log(req.body.name);
-  makeGroups(client, req.body.name)
   res.render('index');
 });
+
+app.post('/', async function(req, res){
+  console.log(req.body)
+})
+
+app.post('/new_room', async function(req, res){
+  console.log(req.body)
+
+  if(req.body.room_name != null){
+    let room_name = req.body.room_name;
+    let group_name = req.body.group;
+    makeRoom(client, group_name, room_name);
+  }
+  
+  let groups = await getGroups(client);  
+  let data = {groups: groups}
+  console.log('reload users')
+  
+  io.emit('reload-groups', data);
+  res.redirect('/')
+})
+
 
 async function main(){
   const url = "mongodb+srv://admin:nxLMpNZw4Gxejkw1@cluster0-v7p6o.mongodb.net/test?retryWrites=true&w=majority";
@@ -44,7 +82,7 @@ async function getGroups(client){
 }
 
 async function makeGroups(client, group_name){
-  await client.db('discord-clone').collection('group').update(
+  await client.db('discord-clone').collection('group').updateOne(
     // condition that this doesn't exist
     {name: group_name},
     // if it doesn't exist, implement this in addition
@@ -54,5 +92,14 @@ async function makeGroups(client, group_name){
     )
 }
 
+async function makeRoom(client, group_name, room_name){
+  await client.db('discord-clone').collection('group').updateOne(
+    // condition that this doesn't exist
+    {name: group_name},
+    // if it doesn't exist, implement this in addition
+    { $addToSet: {rooms: [room_name]}},
+    { upsert : true })
+}
 
-app.listen(process.env.PORT || 3000 );
+
+http.listen(process.env.PORT || 3000 );
--- Project/public/script/script.js
+++ Project/public/script/script.js
@@ -1,4 +1,111 @@
-const groups = document.getElementById("groups");
-<% data.rooms.forEach((id, content) =>{ %>
-    console.log(content)
-});
+window.onload = function (){
+    const group_container = document.getElementById('groups');
+    const room_container = document.getElementById('chatrooms')
+    const new_group_form = document.getElementById('new-group-form')
+    const new_group_form_input = document.getElementById("group-form-name")
+    let groupList;
+    let selectedGroup = null;
+    let selectedRoom = null;
+
+    const socket = io();
+
+    function loadPage(){
+        socket.emit('load-page')
+    }
+
+    socket.on('load-groups', e=>{
+        groupList = e.groups;
+        loadGroups(e.groups)
+    })
+
+    socket.on('reload-groups', e=>{
+        console.log('reloading groups')
+        group_container.innerHTML = ''
+        let groups_content = e.groups;
+        loadGroups(groups_content)
+        
+    })
+    
+    new_group_form.addEventListener('submit', function(e){
+        e.preventDefault();
+        socket.emit('create-group', new_group_form_input.value);
+    });
+
+
+    loadPage();
+    
+    // prints the groups name to the page as a list
+    function loadGroups(groups){
+        group_container.innerHTML = '';
+        groups.forEach(content => { 
+            let element = document.createElement('h5');
+            element.innerHTML = content.name;
+            element.onclick = function(){
+                if(selectedGroup != null){
+                    selectedGroup.style = 'color:black';
+                }
+                groupSelect(content.name)
+                //deselectGroups();
+                element.style = 'color:white'
+                selectedGroup = element;
+            }
+            group_container.appendChild(element);
+        });
+    }
+
+    function groupSelect(group){
+        groupList.forEach(content => { 
+            if(content.name == group){
+                //console.log(group, content.rooms);
+                loadChatrooms(group, content.rooms);
+            }
+        });
+    }
+
+    function loadChatrooms(group, rooms){
+        room_container.innerHTML = '';
+        for(let i = 0; i < rooms.length; i++){
+            let element = document.createElement('h5')
+            element.appendChild(document.createTextNode(rooms[i]));
+            room_container.appendChild(element)
+            element.onclick = function(){
+                if(selectedRoom != null){
+                    selectedRoom.style = 'color:black';
+                }
+                selectedRoom = element;
+                element.style = 'color:white'    
+            }
+        }
+        room_container.appendChild(createRoomForm(group));
+    }
+
+
+    function createRoomForm(group){
+        let form = document.createElement('form');
+        form.action = "/new_room";
+        form.id = 'room_form';
+        form.method = 'POST';
+
+        let input = document.createElement('input');
+        input.id = 'new_room';
+        input.type = 'hidden';
+        input.name = 'group'; 
+        input.value = group
+
+        let input2 = document.createElement('input');
+        input2.id = 'new_room';
+        input2.style = 'width: 95%'
+        input2.type = 'text';
+        input2.name = 'room_name'; 
+        input2.placeholder = 'add room'
+
+        form.appendChild(input);
+        form.appendChild(input2);
+        return form;
+    }
+
+
+}
+
+
+
